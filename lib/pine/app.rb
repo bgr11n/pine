3c8702d504c3aa2e370abb9fc1c75f5f977b1901
Bohdan Hryshchenko <bgr11n@gmail.com>
2015-07-14 16:58:34 +0300

    A new start

--- lib/pine/app.rb
+++ lib/pine/app.rb
@@ -1,11 +1,8 @@
 require 'pine/utils'
-require 'pine/environment'
 require 'pine/base'
 require 'better_errors'
 
 module Pine
   class App < Base
-    # TODO: only for development
-    use BetterErrors::Middleware
   end
 end
--- lib/pine/base.rb
+++ lib/pine/base.rb
@@ -1,42 +1,25 @@
-require 'pine/inheritable'
-require 'pine/routing/namespace'
-
 module Pine
   class Base
-    include Inheritable
-
-    inheritable :middlewares, []
-    inheritable :route, Route.new
-
-    def initialize
-      puts "!! " + self.class.route.routes.inspect
-      self.class.middlewares.each { |m, args, blk| builder.use m, *args, &blk }
-      builder.run self.class.route.endpoint
-      @app = builder.to_app
-    end
 
     def call(env)
-      @app.call env
+      [404, { 'Content-type' => 'application/json' }, [env.inspect]]
     end
+  end
 
-    def builder
-      @builder ||= Rack::Builder.new
-    end
-
-    class << self
-      def use middleware, *args, &block
-        self.middlewares << [middleware, args, block]
-      end
-
-      HTTP_VERBS.each do |verb|
-        define_method verb do |path, options={}|
-          route.write verb, path, options
-        end
-      end
-
-      def namespace namespace, &blk
-        Namespace.new self, namespace, &blk
+  # Pine environment utility. Makes avaliable such methods like:
+  # - Pine.env.development?
+  # - Pine.env.production?
+  # - Pine.env.test?
+  class EnvString < String
+    [:production, :development, :test].each do |env|
+      define_method "#{env}?" do
+        self == env.to_s
       end
     end
+  end
+
+  # Pine.env variable to store application environment
+  def self.env
+    @env ||= EnvString.new(ENV['RACK_ENV'] || 'development')
   end
 end
